d4955dcc862a681b7d03f03c464bc81dc51e3e89
Sarah Phillips <sarahp39@uw.edu>
2018-01-23 19:14:52 -0800

    completed simple functions like add, substract, multiply, and divide

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -11,4 +11,48 @@ import Foundation
 // All your work will go in here
 class Calculator {
     
+    func add(lhs: Int, rhs:Int) -> Int{
+        let total = lhs+rhs;
+        return total;
+    }
+    func add (_ args: [Int]) -> Int{
+        var total = 0;
+        for i in args{
+            total += i;
+        }
+        return total;
+    }
+    func subtract(lhs: Int, rhs: Int) -> Int{
+        let total = lhs-rhs;
+        return total;
+    }
+    func multiply(lhs: Int, rhs: Int) -> Int{
+        let total = lhs*rhs;
+        return total
+    }
+    func multiply(_ args: [Int]) -> Int{
+        var total = 1;
+        for i in args{
+            total *= i;
+        }
+        return total;
+    }
+    func divide(lhs: Int, rhs: Int) -> Int{
+        let total = lhs/rhs;
+        return total;
+    }
+    func count(_ args: [Int]) -> Int{
+        var total = 0;
+        for _ in args{
+            total += 1;
+        }
+        return total;
+    }
+    func avg(_ args: [Int]) -> Int{
+        var total = self.add(args);
+        let counter = self.count(args);
+        total = total/counter;
+        return total;
+    }
+    
 }
